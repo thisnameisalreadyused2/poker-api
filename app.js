f9b90ac8531921d05d4195debdcd510635c2ae1d
BoberMod <BoberMod@yandex.ru>
2019-09-14 23:15:34 +0300

    Add registration

--- app.js
+++ app.js
@@ -1,16 +1,59 @@
-let app = require('express')();
-let server = require('http').Server(app);
-let io = require('socket.io')(server);
+const app = require('express')();
+const hat = require('hat');
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
 server.listen(9999);
+teams = new Map();
 
-app.get('/', function (req, res) {
+app.get('/users', function (req, res) {
     res.sendFile(__dirname + '/index.html');
+});
+app.get('/register', function (req, res) {
+    res.sendFile(__dirname + '/register.html')
 });
 
 io.on('connection', function (socket) {
-    socket.emit('news', { hello: 'world' });
-    socket.on('my other event', function (data) {
-        console.log(data);
+    socket.on('requestToken', () => {
+        let token = hat();
+        teams.set(token, new Map([
+            {userStory: "test story"},
+            {state: "voting"},
+            {users: []}
+        ]));
+        socket.emit('receiveToken', {
+            "token" : token
+        });
+    });
+    socket.on('newTeamMember', (data) => {
+        let currentTeam = teams.get(data.token);
+        let currentUserName = data.username;
+
+        if (currentUserName === null) {
+            socket.emit('needRegister');
+            return;
+        }
+
+        currentTeam.get('users').push({
+          name: currentUserName,
+          socket: socket,
+          vote: null
+        });
+    });
+    socket.on('newVote', () => {
+
     });
 });
+
+// let usersManager = io
+//     .of('/users')
+//     .on('connection', function (socket) {
+//         socket.on('newUser', (user) => {
+//             users.push(user);
+//         })
+//     })
+//     .on('disconnect', function (socket) {
+//         socket.on('leaveUser', (user) => {
+//             users = users.filter(u => u.name != user.name && u.token != user.token);
+//         })
+//     });

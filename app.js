40e5464428ba0ba95354a1ae0b7389e757f769e6
BoberMod <BoberMod@yandex.ru>
2019-09-22 21:20:10 +0300

    Implement user story changing and fix user disconnect

--- app.js
+++ app.js
@@ -6,91 +6,111 @@ const io = require('socket.io')(server);
 server.listen(9999);
 teams = new Map();
 
-app.get('/users', function (req, res) {
-    res.sendFile(__dirname + '/index.html');
-});
-app.get('/register', function (req, res) {
-    res.sendFile(__dirname + '/register.html')
-});
-
-io.on('connection', function (socket) {
-    socket.on('requestToken', () => {
-        let token = hat();
-        createNewTeam(token);
-        socket.emit('receiveToken', {
-            "token" : token
-        });
-    });
-    socket.on('newTeamMember', (data) => {
-      let currentTeam = teams.get(data.token);
-      if (currentTeam === undefined){
-        currentTeam = createNewTeam(data.token);
-      }
-
-      let currentUserName = data.username;
-
-      if (currentUserName === null) {
-          socket.emit('needRegister');
-          return;
-      }
-
-      currentTeam.get('users').push({
-        name: currentUserName,
-        socket: socket,
-        vote: null
-      });
-    });
-    socket.on('newVote', (data) => {
-      let currentTeam = teams.get(data.token);
-      let teamUsers = currentTeam.get('users');
-      let userIndex = teamUsers.findIndex(user => user.socket.id === socket.id);
-      teamUsers[userIndex].vote = data.vote;
-
-      let votes = [];
-
-      for (let userId in teamUsers) {
-        if (teamUsers[userId].vote === null) {
-          votes = [];
-          return;
-        }
-        votes.push({
-          key: teamUsers[userId].socket.id,
-          name: teamUsers[userId].name,
-          points: teamUsers[userId].vote,
-        })
-      }
-      for (let userId in teamUsers) {
-        teamUsers[userId].socket.emit('voteEnded', votes);
-      }
-    });
-    socket.on('onRestartVoting', (data) => {
-      let currentTeam = teams.get(data.token);
-      let teamUsers = currentTeam.get('users');
-      for (let userId in teamUsers) {
-        teamUsers[userId].socket.emit('restartVoting');
-        teamUsers[userId].vote = null;
-      }
-    })
-});
-
 function createNewTeam(token) {
   let newTeam = new Map();
   newTeam.set("userStory", "User story is not selected");
   newTeam.set("state", "Planning");
   newTeam.set("users", []);
   teams.set(token, newTeam);
+
   return newTeam;
 }
 
-// let usersManager = io
-//     .of('/users')
-//     .on('connection', function (socket) {
-//         socket.on('newUser', (user) => {
-//             users.push(user);
-//         })
-//     })
-//     .on('disconnect', function (socket) {
-//         socket.on('leaveUser', (user) => {
-//             users = users.filter(u => u.name != user.name && u.token != user.token);
-//         })
-//     });
+function getCurrentTeam(token) {
+  let currentTeam = teams.get(token);
+  if (currentTeam === undefined) {
+    currentTeam = createNewTeam(token);
+  }
+
+  return currentTeam;
+}
+
+io.on('connection', function (socket) {
+  socket.on('requestToken', () => {
+      let token = hat();
+      createNewTeam(token);
+      socket.emit('receiveToken', {
+          "token" : token
+      });
+  });
+
+  socket.on('newTeamMember', (data) => {
+    let currentTeam = getCurrentTeam(data.token);
+    let currentUserName = data.username;
+
+    if (currentUserName === null) {
+        socket.emit('needRegister');
+        return;
+    }
+
+    currentTeam.get('users').push({
+      name: currentUserName,
+      socket: socket,
+      vote: null
+    });
+  });
+
+  socket.on('newVote', (data) => {
+    let currentTeam = getCurrentTeam(data.token);
+    let teamUsers = currentTeam.get('users');
+    let userIndex = teamUsers.findIndex(user => user.socket.id === socket.id);
+    teamUsers[userIndex].vote = data.vote;
+
+    let votes = [];
+
+    teamUsers.forEach((user) => {
+      if (user.vote === null) {
+        votes.length = 0;
+        return;
+      }
+
+      votes.push({
+        key: user.socket.id,
+        name: user.name,
+        points: user.vote,
+      })
+    });
+
+    teamUsers.forEach((user) => {
+      user.socket.emit('voteEnded', votes);
+    });
+  });
+
+  socket.on('onRestartVoting', (data) => {
+    let currentTeam = getCurrentTeam(data.token);
+    let teamUsers = currentTeam.get('users');
+
+    teamUsers.forEach((user) => {
+      user.socket.emit('restartVoting');
+      user.vote = null;
+    });
+  });
+
+  socket.on('changeUserStory', (data) => {
+    let currentTeam = getCurrentTeam(data.token);
+    let teamUsers = currentTeam.get('users');
+
+    teamUsers.forEach((user) => {
+      user.socket.emit('onChangeUserStory', data);
+    })
+  });
+
+  socket.on('disconnect', () => {
+    let currentTeam = null;
+    let currentUser = null;
+
+    teams.forEach((team) => {
+      team.get('users').forEach((departedUser) => {
+        if (departedUser.socket.id === socket.id) {
+          currentTeam = team;
+          currentUser = departedUser;
+        }
+      });
+    });
+
+    if (currentTeam && currentUser) {
+      console.log("User %s left", currentUser.name);
+      currentTeam.get('users').filter((user) => user.socket.id !== currentUser.socket.id);
+    }
+  });
+})
